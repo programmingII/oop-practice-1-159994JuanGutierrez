165928926cdb64f5c32a8d68a17bc1dc6869d43a
159994JuanGutierrez <46794406+159994JuanGutierrez@users.noreply.github.com>
2019-02-05 13:34:13 -0700

    Add files via upload

---
+++ Ejercicio1_Helloworld.java
@@ -0,0 +1,9 @@
+// Juan Pablo Gutierrez Ceballos 12:35
+
+public class Ejercicio1_Helloworld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello\nJuan Pablo Gutierrez Ceballos");
+	}
+}
